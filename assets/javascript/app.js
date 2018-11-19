d9b3283cab6a067d7e9eb620c291b3f81a66f6e0
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-11-19 13:24:54 -0500

    adding questions and slides

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,27 @@
 //Creating quiz questions
 var quizArray = [
 {
-    question: "Who steals the dino DNA for profit in Jurassic Park?";
-    answers: "Alan Grant, Ian Malcom, John Hammond, Dennis Nedry";
-    correct: "Dennis Nedry";
+    question: "Who steals the dino DNA for profit in Jurassic Park?"
+    answers: "Alan Grant, Ian Malcom, John Hammond, Dennis Nedry"
+    correct: "Dennis Nedry"
 },
 
 {
-    question: "What do they call a Quarter Pounder with cheese in France?";
-    answers: "Le Big Mac, Kilogram, Whopper, Royale with cheese";
-    correct: "Royale with cheese";
+    question: "What do they call a Quarter Pounder with cheese in France?"
+    answers: "Le Big Mac, Kilogram, Whopper, Royale with cheese"
+    correct: "Royale with cheese"
 },
 
 {
-    question: "Who is the main character in Breaking Bad";
-    answers: "Walter White, Gus Fring, Saul Goodman, Hank Schrader";
-    correct: "Walter White";
+    question: "Who is the main character in Breaking Bad?"
+    answers: "Walter White, Gus Fring, Saul Goodman, Hank Schrader"
+    correct: "Walter White"
+},
+
+{
+    question: "What is Batman's real name?"
+    answers: "Clark Kent, Bruce Wayne, Peter Parker, Bruce Banner"
+    correct: "Bruce Wayne"
 },
 
 ]
