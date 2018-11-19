ee29419b4d890ba10d9e56ae4c3a817a261c6c63
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-11-19 15:52:11 -0500

    making divs and id's in html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,38 @@
 //Creating quiz questions
 var quizArray = [
 {
-    question: "Who steals the dino DNA for profit in Jurassic Park?"
-    answers: "Alan Grant, Ian Malcom, John Hammond, Dennis Nedry"
-    correct: "Dennis Nedry"
+    question: "Who steals the dino DNA for profit in Jurassic Park?",
+    answers: "Alan Grant, Ian Malcom, John Hammond, Dennis Nedry",
+    correct: "Dennis Nedry",
 },
 
 {
-    question: "What do they call a Quarter Pounder with cheese in France?"
-    answers: "Le Big Mac, Kilogram, Whopper, Royale with cheese"
-    correct: "Royale with cheese"
+    question: "What do they call a Quarter Pounder with cheese in France?",
+    answers: "Le Big Mac, Kilogram, Whopper, Royale with cheese",
+    correct: "Royale with cheese",
 },
 
 {
-    question: "Who is the main character in Breaking Bad?"
-    answers: "Walter White, Gus Fring, Saul Goodman, Hank Schrader"
-    correct: "Walter White"
+    question: "Who is the main character in Breaking Bad?",
+    answers: "Walter White, Gus Fring, Saul Goodman, Hank Schrader",
+    correct: "Walter White",
 },
 
 {
-    question: "Who is Batman's mortal enemy?"
-    answers: "Thanos, Joker, Lex Luther, Mega mind"
-    correct: "Joker"
+    question: "Who is Batman's mortal enemy?",
+    answers: "Thanos, Joker, Lex Luther, Mega mind",
+    correct: "Joker",
 },
 
 ]
+
+startButton
+
+function on.click
+
+setTimeout
+
+for loop
 
 
 $( document.body ).click(function () {
